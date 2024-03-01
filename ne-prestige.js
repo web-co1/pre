5dae8f6a70d26b85bc7bd1898c42efc4696b8f90
Dev <86075203+web-co1@users.noreply.github.com>
2024-03-01 05:45:32 +0500

    Create ne-prestige.js

---
+++ ne-prestige.js
@@ -0,0 +1,325 @@
+var pre__size_72 = {
+  white: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b1018ea62b2192cc7adb2c_image%2021%20(7).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8cffe4aa293e5fca73a63_Prestige%2060%20w%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8d1511b098f0740d036da_Prestige%2060%20w%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8d1bb0b7b07d9ce79a194_Prestige%2060%20w%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8d221325d4879778e3803_Prestige%2060%20w%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65cb4e99bc40b6a24558671c_Prestige%2060%20w%203_1%20(1).png",
+    ],
+  },
+  grey: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b1008a2e5f15e04587c8b4_image%2021%20(4).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8d6196348e831379476af_Prestige%2060%20gray%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8d68e8e01ed41a9c56982_Prestige%2060%20gray%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8d6db6348e8313794cc29_Prestige%2060%20gray%20%202_1.png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8d73a485cb9900dc88be2_Prestige%2060%20gray%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65cb514103a13c19701d9f57_Prestige%2060%20gray%203_1%20(1).png",
+    ],
+  },
+
+  dark: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b100c4d025ce4e461d2f55_image%2021%20(5).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8d7eb3203b6859542f9f1_Prestige%2060%20b%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8d81dcc917f11fc3d44ea_Prestige%2060%20b%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8d8795b8d06114ce05827_Prestige%2060%20b%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8da3e06415672602da140_Prestige%2060%20b%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65cb51baa08b2efa792e78d8_Prestige%2060%20b%203_1%20(1).png",
+    ],
+  },
+
+  green: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b1013b0d68d2c04d000e66_image%2021%20(6).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8db5da3bd01ebf43761fe_Prestige%2060%20g%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8dc995dcfd3322ada719b_Prestige%2060%20g%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8dd50b02e7eb54f7ca1d1_Prestige%2060%20g%202_1%20(3).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8de3fb28fe27c777432ab_Prestige%2060%20g%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65cb53fba08b2efa792fb6f2_Prestige%2060%20g%203_1%20(1)%20(1).png",
+    ],
+  },
+};
+
+var pre__size_60 = {
+  white: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b1018ea62b2192cc7adb2c_image%2021%20(7).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8cffe4aa293e5fca73a63_Prestige%2060%20w%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8d1511b098f0740d036da_Prestige%2060%20w%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8d1bb0b7b07d9ce79a194_Prestige%2060%20w%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8d221325d4879778e3803_Prestige%2060%20w%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65cb4e99bc40b6a24558671c_Prestige%2060%20w%203_1%20(1).png",
+    ],
+  },
+  grey: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b1008a2e5f15e04587c8b4_image%2021%20(4).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8d6196348e831379476af_Prestige%2060%20gray%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8d68e8e01ed41a9c56982_Prestige%2060%20gray%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8d6db6348e8313794cc29_Prestige%2060%20gray%20%202_1.png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8d73a485cb9900dc88be2_Prestige%2060%20gray%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65cb514103a13c19701d9f57_Prestige%2060%20gray%203_1%20(1).png",
+    ],
+  },
+
+  dark: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b100c4d025ce4e461d2f55_image%2021%20(5).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8d7eb3203b6859542f9f1_Prestige%2060%20b%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8d81dcc917f11fc3d44ea_Prestige%2060%20b%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8d8795b8d06114ce05827_Prestige%2060%20b%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8da3e06415672602da140_Prestige%2060%20b%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65cb51baa08b2efa792e78d8_Prestige%2060%20b%203_1%20(1).png",
+    ],
+  },
+
+  green: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b1013b0d68d2c04d000e66_image%2021%20(6).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8db5da3bd01ebf43761fe_Prestige%2060%20g%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8dc995dcfd3322ada719b_Prestige%2060%20g%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8dd50b02e7eb54f7ca1d1_Prestige%2060%20g%202_1%20(3).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c8de3fb28fe27c777432ab_Prestige%2060%20g%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65cb53fba08b2efa792fb6f2_Prestige%2060%20g%203_1%20(1)%20(1).png",
+    ],
+  },
+};
+
+var pre__size_48 = {
+  white: {
+     mainImg:
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c28252918f4c6d9bd48d09_image%2021%20(4)%20(1).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e10378a047b7c479ab3599_Prestige%2048%20w%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e103a0e19e5f02958bf9c5_Prestige%2048%20w%202_2%20(1).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65e0eadc577f04001fe4230a_Soho%2048%20w%202_1%20(1)-p-1600.png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65e0eafa098f7a096a515448_Soho%2048%20w%201_1%20(1)-p-1600.png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e114c982c772422db174cc_Prestige%2048%20w%203%20(1).png",
+    ],
+  },
+  grey: {
+    mainImg:
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c283d8b71ed6f330403f82_image%2021%20(5)%20(1).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e11699e6e4b322d59e27bc_Prestige%2048%20gray%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e116cc97ebe1032207893b_Prestige%2048%20gray%202_2%20(1).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65e0ebfe3f8d1ed5c0dd4e4e_Soho%2048%20gray%202_1%20(1)-p-1600.png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65e0ec0f07456f1ee73c3f2c_Soho%2048%20gray%201_1%20(1)-p-1600.png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e118b8b93a5410ee367f51_Prestige%2048%20gray%203%20(1).png",
+    ],
+  },
+  dark: {
+    mainImg:
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c286e7a6a3a5bc0e6e4f88_image%2021%20(6).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e1198191de7762ab2aedb8_Prestige%2048%20b%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e11998ad2439a414b16d14_Prestige%2048%20b%202_2%20(1).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65e0ece3c0b7991d0b4b36d9_Soho%2048%20b%202_1%20(1)-p-1600.png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65e0ecf69a98a72494ced071_Soho%2048%20b%201_1%20(1)-p-1600.png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e11b51cc34d594d9b3b350_Prestige%2048%20b%203%20(1).png",
+    ],
+  },
+  green: {
+    mainImg:
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c2887959bfdd4421519b51_image%2021%20(7).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e11c3a0afd0ad056271565_Prestige%2048%20g%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e11c540e29995b56c79805_Prestige%2048%20g%202_2%20(1).png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65e0ee15b9249f5e5d6f09a9_Soho%2048%20g%202_1%20(1)-p-1600.png",
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65e0ee25340eae152663e88b_Soho%2048%20g%201_1%20(1)-p-1600.png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e11dcccd85cec709d51583_Prestige%2048%20g%203%20(1).png",
+    ],
+  },
+};
+
+var pre__size_36 = {
+  white: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b938e657dc40b61550a53f_image%2021%20(4).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e11ff40afd0ad05628e97f_Prestige%2036%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e12009ab44e009db781299_Prestige%2036%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e120219f3b032ce186006c_Prestige%2036%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e12036ad2439a414b4c008_Prestige%2036%201_1%20(1)%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e120488d39e2b6d85476db_Prestige36%20w%203_1%20(1).png",
+    ],
+  },
+  grey: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b93f6bb48367547565385d_image%2021%20(5).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e121660afd0ad05629c779_Prestige36%20grey%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e12187b4fb76714ce63dad_Prestige36%20grey%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e121aa0e29995b56ca3b95_Prestige36%20grey%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e121c9cc34d594d9b74cae_Prestige36%20grey%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e121da0d76e91f761f8cb3_Prestige36%20grey%203_1%20(1).png",
+    ],
+  },
+  dark: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b9423c806f43b4bbdbd535_image%2021%20(6).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e122d794e29b8220b17140_Prestige36%20b%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e122ea7eb1184bfc3cccb6_Prestige36%20b%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e122fd59166fc7b743ac8d_Prestige36%20b%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e1231673f9c38869955070_Prestige36%20b%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e1232c84d7dde30f560c91_Prestige36%20b%203_1%20(1).png",
+    ],
+  },
+  green: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b947d2e8ed0b28770a570e_image%2021%20(7).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e12456ad2439a414b71cbf_Prestige36%20g%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e12466022b6c22511ee25e_Prestige36%20g%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e1247b7612000d452f18d0_Prestige36%20g%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e1248d691848ad99ec59d6_Prestige36%20g%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e124a039ef6cc36cd961e7_Prestige36%20g%203_1%20(1).png",
+    ],
+  },
+};
+
+var pre__size_30 = {
+  white: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b938e657dc40b61550a53f_image%2021%20(4).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e11ff40afd0ad05628e97f_Prestige%2036%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e12009ab44e009db781299_Prestige%2036%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e120219f3b032ce186006c_Prestige%2036%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e12036ad2439a414b4c008_Prestige%2036%201_1%20(1)%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e120488d39e2b6d85476db_Prestige36%20w%203_1%20(1).png",
+    ],
+  },
+  grey: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b93f6bb48367547565385d_image%2021%20(5).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e121660afd0ad05629c779_Prestige36%20grey%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e12187b4fb76714ce63dad_Prestige36%20grey%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e121aa0e29995b56ca3b95_Prestige36%20grey%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e121c9cc34d594d9b74cae_Prestige36%20grey%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e121da0d76e91f761f8cb3_Prestige36%20grey%203_1%20(1).png",
+    ],
+  },
+  dark: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b9423c806f43b4bbdbd535_image%2021%20(6).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e122d794e29b8220b17140_Prestige36%20b%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e122ea7eb1184bfc3cccb6_Prestige36%20b%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e122fd59166fc7b743ac8d_Prestige36%20b%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e1231673f9c38869955070_Prestige36%20b%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e1232c84d7dde30f560c91_Prestige36%20b%203_1%20(1).png",
+    ],
+  },
+  green: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b947d2e8ed0b28770a570e_image%2021%20(7).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e12456ad2439a414b71cbf_Prestige36%20g%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e12466022b6c22511ee25e_Prestige36%20g%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e1247b7612000d452f18d0_Prestige36%20g%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e1248d691848ad99ec59d6_Prestige36%20g%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e124a039ef6cc36cd961e7_Prestige36%20g%203_1%20(1).png",
+    ],
+  },
+};
+
+
+// Get references to size and color buttons
+const sizeButtons04 = document.querySelectorAll("#prestige .size-btn");
+const colorButtons04 = document.querySelectorAll("#prestige .variant-btn");
+
+// Function to update images based on selected size and color
+function updateImages04(size, color) {
+  const sizeObject = window[`pre__size_${size}`];
+
+  const colorObject = sizeObject[color];
+
+  if (sizeObject && colorObject) {
+    // Set main image URL
+    var mainImageElement = document.querySelector(
+      "#prestige .images-block img"
+    );
+    if (mainImageElement) {
+      mainImageElement.src = colorObject.mainImg;
+      mainImageElement.srcset = colorObject.mainImg;
+    }
+
+    // Loop through sliders and set new src attributes
+    var sliderImages = document.querySelectorAll("#prestige .slide-item img ");
+
+    if (colorObject.sliders.length > 0) {
+      for (var i = 0; i < sliderImages.length; i++) {
+        var index = i % colorObject.sliders.length;
+        var sliderImage = colorObject.sliders[index];
+
+        if (sliderImages[i]) {
+          sliderImages[i].src = sliderImage;
+          sliderImages[i].srcset = sliderImage;
+        }
+      }
+    }
+  }
+}
+
+// Function to handle size button click
+function handleSizeButtonClick04(size) {
+  // Remove 'w--current' class from all size buttons
+  sizeButtons04.forEach((button) => button.classList.remove("size__current"));
+
+  // Add 'w--current' class to the clicked size button
+  this.classList.add("size__current");
+
+  const Color = document.querySelector("#prestige .variant-btn.active")
+    .classList[2];
+
+  // Update images based on the selected size and default color
+  updateImages04(size, Color);
+}
+
+// Function to handle color button click
+function handleColorButtonClick04(color) {
+  // Remove 'active' class from all color buttons
+  colorButtons04.forEach((button) => button.classList.remove("active"));
+
+  // Add 'active' class to the clicked color button
+  this.classList.add("active");
+
+  // Get the selected size button with 'w--current' class
+  const selectedSizeButton = document.querySelector(
+    "#prestige .size-btn.size__current"
+  );
+
+  // If a size button is selected, update images based on the selected size and color
+  if (selectedSizeButton) {
+    const selectedSize = selectedSizeButton.innerText.trim().split("”")[0]; //output 72"
+    updateImages04(selectedSize, color);
+  }
+}
+
+// Attach click event listeners to size buttons
+sizeButtons04.forEach((button) =>
+  button.addEventListener("click", function () {
+    handleSizeButtonClick04.call(this, this.innerText.trim().split("”")[0]);
+  })
+);
+
+// Attach click event listeners to color buttons
+colorButtons04.forEach((button) =>
+  button.addEventListener("click", function () {
+    handleColorButtonClick04.call(this, this.classList[2]); // Assuming the color class is the second class in the classList
+  })
+);
